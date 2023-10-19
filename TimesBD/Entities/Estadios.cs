4532237aac16862facc814e1283ad66af145324a
CaioCeesaar <caio2001lc@gmail.com>
2023-10-19 01:45:20 -0300

    Implementação de novas classes

---
+++ TimesBD/Entities/Estadios.cs
@@ -0,0 +1,19 @@
+﻿namespace TimesBD.Entities
+{
+    public class Estadios
+    {
+        protected Estadios() { }
+
+        public Estadios(string nome, int enderecoId)
+        {
+            Nome = nome;
+            EnderecoId = enderecoId;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int EnderecoId { get; set; }
+        
+        public Endereco EnderecoEstadio { get; set; }
+    }
+}
---
+++ TimesBD/Entities/Jogo.cs
@@ -0,0 +1,20 @@
+﻿namespace TimesBD.Entities
+{
+    public class Jogo
+    {
+            protected Jogo() { }
+
+            public Jogo(DateTime hora, DateTime data, int estadioId)
+            {
+                Hora = hora;
+                Data = data;
+                EstadioId = estadioId;
+            }
+
+            public int Id { get; set; }
+            public DateTime Hora { get; set; }
+            public DateTime Data { get; set; }
+            public int EstadioId { get; set; }
+        
+    }
+}
