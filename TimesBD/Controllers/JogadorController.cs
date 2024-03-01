721715674969f978e58fadbd11565a7b069559fa
CaioCeesaar <caio2001lc@gmail.com>
2024-03-01 11:23:25 -0300

    Revert "Merge branch 'master' into dev"
    This reverts commit 22ecf2e44a4424231e77c6fc650f89189780c44c, reversing
    changes made to 805c7e83d569924d04b5dd581831474656418cf1.

--- TimesBD/Controllers/JogadorController.cs
+++ TimesBD/Controllers/JogadorController.cs
@@ -1,4 +1,4 @@
-using System.Text.Json;
+﻿using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using TimesBD.Entities;
 using TimesBD.Business;
--- TimesBD/Controllers/TimeController.cs
+++ TimesBD/Controllers/TimeController.cs
@@ -1,4 +1,4 @@
-using System.Text.Json;
+﻿using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using TimesBD.Business;
 using TimesBD.Entities;
